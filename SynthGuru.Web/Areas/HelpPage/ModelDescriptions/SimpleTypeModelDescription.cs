b0080551d61bbb0530abc153c1273e5faf1bd505
Morten Kristensen <godselevator@gmail.com>
2018-09-25 13:55:33 +0200

    Commit before refactor

---
+++ SynthGuru.Web/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SynthGuru.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ SynthGuru.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SynthGuru.Web.WebApiApplication" Language="C#" %>
